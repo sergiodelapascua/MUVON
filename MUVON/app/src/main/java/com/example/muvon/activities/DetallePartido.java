b37049042d0db99f7153e8586816f1be37999091
Sergio de la Pascua <sergiodelapascuaroca@gmail.com>
2020-05-24 15:17:06 +0200

    Interfaz Android

---
+++ MUVON/app/src/main/java/com/example/muvon/activities/DetallePartido.java
@@ -0,0 +1,13 @@
+package com.example.muvon.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetallePartido extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_partido);
+    }
+}
